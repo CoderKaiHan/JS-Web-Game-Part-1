20c1fed55ed6494a999d4c6a17fe1c8fddacdabd
CoderKaiHan <hankaicoder@gmail.com>
2023-11-16 19:16:46 -0500

    added pine tree

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
+
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let greenPineTree = document.createElement('img')
+greenPineTree.src = 'assets/pine-tree.png'
+greenPineTree.style.position = 'fixed'
+greenPineTree.style.left = '450px'
+greenPineTree.style.bottom = '200px'
+document.body.append(greenPineTree)
